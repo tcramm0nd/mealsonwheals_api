21b36bb65a113dd02538ccc0388acaf40e71d716
tcramm0nd <25987791+tcramm0nd@users.noreply.github.com>
2022-01-22 19:52:41 -0500

    Initial commit

---
+++ Models/FoodTruckLocation.cs
@@ -0,0 +1,14 @@
+namespace MealsOnWheels.Models
+{
+    public class FoodTruckLocation
+    // location for a particular food truck, on a particular DT
+    {
+        public long ID {get; set; }
+        public long? FoodTruckID {get; set; }
+        public string Address {get; set; } = string.Empty;
+        public DateTime TimeOpen {get; set; }
+        public DateTime TimeClosed {get; set; }
+        public double Latitude {get; set; }
+        public double Longitude {get; set; }
+    }
+} 
